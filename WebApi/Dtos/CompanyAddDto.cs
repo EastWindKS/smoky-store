ec88faf49d51f2d4f640e07425e5177ed453822d
EastWind <awfulaaa@gmail.com>
2020-08-19 20:33:19 +0300

    test commit

--- WebApi/Dtos/CompanyAddDto.cs
+++ WebApi/Dtos/CompanyAddDto.cs
@@ -4,10 +4,10 @@ namespace WebApi.Dtos
 {
     public class CompanyAddDto
     {
-        
         public string CompanyName { get; set; }
-        
+
         public string ImgUrl { get; set; }
-        public string [] Strengths { get; set; }
-       }
+        public string[] Strengths { get; set; }
+        public string Valera { get; set; }
+    }
 }
